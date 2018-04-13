1d3c347fb9d90a69a2a572fa4762c406ebb0e382
Aaron Na <syn228@nyu.edu>
2018-04-13 20:12:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,8 @@ def oxford_comma(array)
   array.join(" and ")
   elsif array.length == 3
   "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    "#{array[0]}, #{array[1]}, #{array[2]} and #{array[3]}"
 end
   
 end
