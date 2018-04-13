5eb09a65685fc444890bef1e78e03e2d8cc2bead
Aaron Na <syn228@nyu.edu>
2018-04-13 20:15:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.length == 3
   "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
-    "#{array[0]}, #{array[1]}, #{..}, and #{array[max]}"
+    "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, and #{array[4]}"
 end
   
 end
