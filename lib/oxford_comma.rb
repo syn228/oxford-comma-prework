7ab9fac31cacbe30c86c4bbc80e8417048a67639
Aaron Na <syn228@nyu.edu>
2018-04-13 20:25:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+  array.join
+  elsif array.length == 2
+  array.join(" and ")
+  elsif array.length == 3
+  "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length >= 4
+  array.push("and #{array[-1]}")
+  array.delete_at(-2)
+  array.join(", ")
+end
+  
 end
